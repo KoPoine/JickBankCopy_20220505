2c85e759adb66df688615bba7edc2ca3d9e5a41a
mincheul Kim <kmc901206@gmail.com>
2022-05-06 02:40:10 +0900

    9. 세부 방 보여줄 액티비티 생성

---
+++ app/src/main/java/com/nepplus/jickbankcopy_20220505/DetailRoomActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.jickbankcopy_20220505
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailRoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_room)
+    }
+}
