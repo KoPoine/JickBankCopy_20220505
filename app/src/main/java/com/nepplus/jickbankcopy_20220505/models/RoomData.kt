fac85b9ba90dcd8182f56535fc847d07ee40974d
mincheul Kim <kmc901206@gmail.com>
2022-05-06 02:08:26 +0900

    3. 방표현용 데이터 클래스 추가

---
+++ app/src/main/java/com/nepplus/jickbankcopy_20220505/models/RoomData.kt
@@ -0,0 +1,9 @@
+package com.nepplus.jickbankcopy_20220505.models
+
+class RoomData (
+    val price : Int,
+    val address : String,
+    val level : Int,
+    val description : String,
+        ) {
+}
