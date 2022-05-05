f636033e37a6fa5dbd52c4424005f5b49c4e9f26
mincheul Kim <kmc901206@gmail.com>
2022-05-06 02:58:13 +0900

    10. detail 화면 꾸미기 완료

--- app/src/main/java/com/nepplus/jickbankcopy_20220505/DetailRoomActivity.kt
+++ app/src/main/java/com/nepplus/jickbankcopy_20220505/DetailRoomActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.nepplus.jickbankcopy_20220505.models.RoomData
+import kotlinx.android.synthetic.main.activity_detail_room.*
+import java.text.DecimalFormat
 
 class DetailRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,29 @@ class DetailRoomActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail_room)
 
         val roomData = intent.getSerializableExtra("roomData") as RoomData
+
+        val formatter = DecimalFormat("#,###")
+
+        val resultPrice = if (roomData.price < 10000) {
+            formatter.format(roomData.price)
+        } else {
+            val over = roomData.price / 10000
+            val remain = roomData.price % 10000
+            "${over}억${formatter.format(remain)}"
+        }
+
+        val resultLevel =
+            when {
+                roomData.level > 0 -> { "${roomData.level}층" }
+                roomData.level < 0 -> {
+                    val level = roomData.level * -1
+                    "지하${level}층"
+                }
+                else -> {"반지하"}
+            }
+
+        priceTxt.text = resultPrice
+        addressTxt.text = "${roomData.address}, $resultLevel"
+        descriptionTxt.text = roomData.description
     }
 }
