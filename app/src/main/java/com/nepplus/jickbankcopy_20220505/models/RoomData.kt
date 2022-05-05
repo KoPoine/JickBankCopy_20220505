680836951a697a3dd49ad4f61b2cb37054605212
mincheul Kim <kmc901206@gmail.com>
2022-05-06 02:38:46 +0900

    8. 표현용 데이터 클래스 직렬화

--- app/src/main/java/com/nepplus/jickbankcopy_20220505/models/RoomData.kt
+++ app/src/main/java/com/nepplus/jickbankcopy_20220505/models/RoomData.kt
@@ -1,9 +1,11 @@
 package com.nepplus.jickbankcopy_20220505.models
 
+import java.io.Serializable
+
 class RoomData (
     val price : Int,
     val address : String,
     val level : Int,
     val description : String,
-        ) {
+        ) : Serializable {
 }
