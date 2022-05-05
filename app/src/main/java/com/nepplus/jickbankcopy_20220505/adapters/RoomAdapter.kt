2dd465254d9295092404868b4f2e4f60aae64258
mincheul Kim <kmc901206@gmail.com>
2022-05-06 02:11:40 +0900

    4. 어댑터 클래스 기초 작업

---
+++ app/src/main/java/com/nepplus/jickbankcopy_20220505/adapters/RoomAdapter.kt
@@ -0,0 +1,25 @@
+package com.nepplus.jickbankcopy_20220505.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.nepplus.jickbankcopy_20220505.R
+import com.nepplus.jickbankcopy_20220505.models.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId : Int,
+    val mList : ArrayList<RoomData>,
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+        val row = tempRow!!
+        return row
+    }
+}
